e002b2b811d48e73b057fa7bd211cef57f90bc1a
elodie-boweren <elodie.boweren@laplateforme.io>
2024-12-03 11:33:54 +0100

    chiffres dans grid

--- grid.py
+++ grid.py
@@ -1,8 +1,15 @@
+grid = range(9) #grille avec chiffres
+
 def display_grid(grid):
-    """Affiche la grille du jeu."""
+    #Affiche la grille du jeu.
     print("\n")
     for i in range(0, 9, 3):
         print(f" {grid[i]} | {grid[i + 1]} | {grid[i + 2]} ")
         if i < 6:
             print("---|---|---")
     print("\n")
+
+
+
+
+
