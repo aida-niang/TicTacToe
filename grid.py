795ced0d781c44b4e81b39f3e43cad1a84502bd0
aida-niang <aida.niang@laplateforme.io>
2024-12-04 15:27:26 +0100

    maj grid

--- grid.py
+++ grid.py
@@ -1,6 +1,4 @@
-grid = range(9)
-
-def display_grid(grid):  #Affiche la grille du jeu.
+def display_grid(grid): #Affiche la grille de jeu.
     print("\n")
     for i in range(0, 9, 3):
         print(f" {grid[i]}  |  {grid[i + 1]}  |  {grid[i + 2]} ")
@@ -8,3 +6,8 @@ def display_grid(grid):  #Affiche la grille du jeu.
             print("----|-----|----")
     print("\n")
 
+
+def update_grid(grid, position, mark): #Met à jour la grille à la position donnée avec le symbole du joueur.
+    if 0 <= position < len(grid) and isinstance(grid[position], int):  # Vérifie que la case est jouable (contient un numéro)
+        grid[position] = mark
+   
